f5db4ddca876bff5644fb49aadeb2ec842438c8d
adenugbamichael <adenugbamicke@gmail.com>
2024-06-22 14:19:58 +0100

    Resolving relations

--- api/src/schema/types/task.js
+++ api/src/schema/types/task.js
@@ -5,27 +5,30 @@ import {
   GraphQLInt,
   GraphQLNonNull,
   GraphQLList,
-  } from 'graphql';
+} from "graphql"
+import User from "./user"
 
+const Task = new GraphQLObjectType({
+  name: "Task",
+  fields: {
+    id: { type: new GraphQLNonNull(GraphQLID) },
+    content: { type: new GraphQLNonNull(GraphQLString) },
+    tags: {
+      type: new GraphQLNonNull(
+        new GraphQLList(new GraphQLNonNull(GraphQLString))
+      ),
+      resolve: (source) => source.tags.split(","),
+    },
+    approachCount: { type: new GraphQLNonNull(GraphQLInt) },
+    createdAt: {
+      type: new GraphQLNonNull(GraphQLString),
+      resolve: (source) => source.createdAt.toISOString(),
+    },
+    author: {
+      type: new GraphQLNonNull(User),
+      resolve: (source, args, { pgApi }) => pgApi.userInfo(source.userId),
+    },
+  },
+})
 
-  const Task = new GraphQLObjectType({
-    name: "Task",
-    fields: {
-      id: {type: new GraphQLNonNull(GraphQLID)},
-      content: {type: new GraphQLNonNull(GraphQLString)},
-      tags: {
-        type: new GraphQLNonNull(
-          new GraphQLList(
-            new GraphQLNonNull(
-              GraphQLString))
-        ),
-        resolve: (source) => source.tags.split(','),
-      },
-      approachCount: { type: new GraphQLNonNull(GraphQLInt)},
-      createdAt: { type: new GraphQLNonNull(GraphQLString),
-        resolve: (source) => source.createdAt.toISOString(),
-      },
-    }
-  })
-
-  export default Task;
+export default Task
