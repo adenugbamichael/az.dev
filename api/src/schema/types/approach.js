0a8e74c62c1f0643a13d670af8656de97aaa281d
adenugbamichael <adenugbamicke@gmail.com>
2024-06-24 11:58:18 +0100

    Using DataLoader  with MongoDB

--- api/src/schema/types/approach.js
+++ api/src/schema/types/approach.js
@@ -2,18 +2,19 @@ import {
   GraphQLID,
   GraphQLObjectType,
   GraphQLString,
+  GraphQLList,
   GraphQLInt,
   GraphQLNonNull,
 } from "graphql"
 import User from "./user"
-import Task from "./task";
-import SearchResultItem from "./search-result-item";
-
+import Task from "./task"
+import SearchResultItem from "./search-result-item"
+import ApproachDetail from "./approach-detail"
 
 const Approach = new GraphQLObjectType({
   name: "Approach",
   interfaces: () => [SearchResultItem],
-  fields: () =>  ({
+  fields: () => ({
     id: { type: new GraphQLNonNull(GraphQLID) },
     content: { type: new GraphQLNonNull(GraphQLString) },
     voteCount: { type: new GraphQLNonNull(GraphQLInt) },
@@ -27,10 +28,16 @@ const Approach = new GraphQLObjectType({
     },
     task: {
       type: new GraphQLNonNull(Task),
-      resolve: (source, args, {loaders}) => 
-        loaders.tasks.load(source.taskId)
-    }
+      resolve: (source, args, { loaders }) => loaders.tasks.load(source.taskId),
+    },
+    detailList: {
+      type: new GraphQLNonNull(
+        new GraphQLList(new GraphQLNonNull(ApproachDetail))
+      ),
+      resolve: (source, args, { loaders }) =>
+        loaders.detailLists.load(source.id),
+    },
   }),
 })
 
-export default Approach;
+export default Approach
--- api/src/server.js
+++ api/src/server.js
@@ -8,10 +8,12 @@ import morgan from "morgan"
 
 import * as config from "./config"
 import pgApiWrapper from "./db/pg-api"
+import mongoApiWrapper from './db/mongo-api';
 import DataLoader from "dataloader"
 
 async function main() {
   const pgApi = await pgApiWrapper()
+  const mongoApi = await mongoApiWrapper()
   const server = express()
   server.use(cors())
   server.use(morgan("dev"))
@@ -28,7 +30,11 @@ async function main() {
       searchResults: new DataLoader((searchTerms) =>
         pgApi.searchResults(searchTerms)
       ),
+      detailLists: new DataLoader((approachIds) =>
+        mongoApi.detailLists(approachIds)
+      ),
     }
+
     graphqlHTTP({
       schema,
       context: { loaders },
